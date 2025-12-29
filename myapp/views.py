a1f3bdc21e61d58aa2e1e2b31058cf27d3d16748
Nitya Shah <ncs110905@gmail.com>
2025-12-29 21:49:01 +0530

    Pages

--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,24 @@ def index(request):
 
 def about(request):
     return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def feature(request):
+    return render(request,'feature.html')
+
+def project(request):
+    return render(request,'project.html')
+
+def service(request):
+    return render(request,'service.html')
+
+def team(request):
+    return render(request,'team.html')
+
+def testimonial(request):
+    return render(request,'testimonial.html')
+
+def error(request):
+    return render(request,'404.html')

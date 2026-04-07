7d805f9796d5ba52108b64e1e6f18cb8327a8d1f
Nitya Shah <ncs110905@gmail.com>
2026-04-07 17:59:52 +0530

    Fix Cloudinary template rendering bug

--- Insight/settings.py
+++ Insight/settings.py
@@ -156,7 +156,6 @@ EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_TIMEOUT = 10
-load_dotenv()
 
 MEDIA_URL = '/media/'
 
@@ -170,4 +169,10 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME', ''),
     'API_KEY': os.environ.get('API_KEY', ''),
     'API_SECRET': os.environ.get('API_SECRET', ''),
+}
+
+CLOUDINARY = {
+    'cloud_name': os.environ.get('CLOUD_NAME', ''),
+    'api_key': os.environ.get('API_KEY', ''),
+    'api_secret': os.environ.get('API_SECRET', ''),
 }
